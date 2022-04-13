bd06c159f47a197ded2685f891bf4b4e25b1013d
蔡武 <caiwu@3xdata.cn>
2022-04-14 03:28:49 +0800

    mark

--- src/componentsDomo/content/index.js
+++ src/componentsDomo/content/index.js
@@ -4,6 +4,7 @@ import { createRef } from '../../createElement'
 export class Content extends Component {
   constructor(props) {
     super(props)
+    this.setState = {}
   }
   render() {
     return <span>content</span>
